12816cd7e2d154c125a1cda67434a5defc46605b
Alastair Brunton <info@simplyexcited.co.uk>
2011-11-01 11:52:43 +0100

    Adding some basic indempotency

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,34 +8,39 @@
 #
 
 
-execute "flush iptables rules" do
-  command "/sbin/iptables -F"
-  user "root"
-  group "root"
-end
+unless File.exists?("/etc/iptables.up.rules")
 
-template "/etc/iptables.up.rules" do
-  source "iptables.up.rules.erb"
-  owner "root"
-  group "root"
-  mode "0755"
-  variables(
-  :ssh_port => node["ipcoffeetables"]["ssh_port"],
-  :custom_ports => node["ipcoffeetables"]["custom_ports"]
-  )
-end
 
-execute "load rules" do
-  command "/sbin/iptables-restore < /etc/iptables.up.rules"
-  user "root"
-  group "root"
-end
+  execute "flush iptables rules" do
+    command "/sbin/iptables -F"
+    user "root"
+    group "root"
+  end
 
-# Startup template to load rules at reboot
+  template "/etc/iptables.up.rules" do
+    source "iptables.up.rules.erb"
+    owner "root"
+    group "root"
+    mode "0755"
+    variables(
+    :ssh_port => node["ipcoffeetables"]["ssh_port"],
+    :custom_ports => node["ipcoffeetables"]["custom_ports"]
+    )
+  end
 
-template "/etc/network/if-pre-up.d/iptables" do
-  source "iptables-pre-up.erb"
-  owner "root"
-  group "root"
-  mode "0775"
+  execute "load rules" do
+    command "/sbin/iptables-restore < /etc/iptables.up.rules"
+    user "root"
+    group "root"
+  end
+
+  # Startup template to load rules at reboot
+
+  template "/etc/network/if-pre-up.d/iptables" do
+    source "iptables-pre-up.erb"
+    owner "root"
+    group "root"
+    mode "0775"
+  end
+
 end
